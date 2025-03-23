17dfaae2b98ebdc37d8d9378a1f711a9d17a5d5f
Mussadiq Ali <musadiq505@gmail.com>
2025-03-24 01:02:54 +0500

    Removed unused Checkout import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import ShopContextProvider from "./Components/Context/ShopContext";
 import "./index.css";
-import Checkout from "./Components/CheckOut/CheckOut";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
